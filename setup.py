2f48303933d3ca412c39f73e359a8708164cc468
Andreas Wuerl <andreas.wuerl@uniscon.com>
2019-09-11 15:56:28 +0200

    fix base image name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="netboot-config",
-    version="0.1.26",
+    version="0.1.27",
     author="Andreas Würl",
     author_email="andreas.wuerl@uniscon.com",
     description="Generator for KIWI based netboot config files",
