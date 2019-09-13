cef000d7b1f41a519a20b2cac015d651d97e4189
Andreas Wuerl <andi@tryb.de>
2019-09-13 15:32:02 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="netboot-config",
-    version="0.1.27",
+    version="0.1.28",
     author="Andreas Würl",
     author_email="andreas.wuerl@uniscon.com",
     description="Generator for KIWI based netboot config files",
